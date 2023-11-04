e6914b7f54415b9e4f9f991d220594c74ce0d210
Nathan Soares Silva <nathan.silva@teros.com.br>
2023-11-03 21:26:47 -0300

    modal

--- FrontEnd/src/components/Menu/index.tsx
+++ FrontEnd/src/components/Menu/index.tsx
@@ -1,6 +1,8 @@
-import { ReactNode } from "react";
-import { AliwangwangOutlined, CameraOutlined, HomeOutlined } from "@ant-design/icons";
+import { ReactNode, useState } from "react";
+import { AliwangwangOutlined, CameraOutlined, HomeOutlined, UploadOutlined } from "@ant-design/icons";
 import { useNavigate, useLocation } from "react-router-dom";
+import { Button, Modal, Upload, message } from "antd";
+import SimpleDialogDemo from "../Dialog";
 
 interface MobileAppBarProps {
   children: ReactNode;
@@ -9,6 +11,7 @@ interface MobileAppBarProps {
 export const MenuComponent: React.FC<MobileAppBarProps> = ({ children }) => {
   const navigate = useNavigate();
   const path = useLocation().pathname;
+
   return (
     <>
       <div className={`${path == "/CameraCapture" && "hidden"} bg-emerald-500  p-2 px-16 z-40 fixed bottom-0 left-0 w-full flex justify-between items-center shadow-md `}>
@@ -20,19 +23,12 @@ export const MenuComponent: React.FC<MobileAppBarProps> = ({ children }) => {
         >
           <HomeOutlined />
         </button>
-        <button
-          className="text-white z-0 text-4xl fixed bottom-2 right-1/2 transform translate-x-1/2 bg-gradient-to-t from-green-800  to-emerald-500  flex justify-center p-4 rounded-full"
-          onClick={() => {
-            navigate("/CameraCapture");
-          }}
-        >
-          <CameraOutlined />
-        </button>
+        <SimpleDialogDemo />
         <button className="text-white text-2xl">
           <AliwangwangOutlined />
         </button>
       </div>
-      <div className="mt-16 overflow-auto">
+      <div className=" overflow-auto">
         {/* Renderiza o componente filho dentro da área de visualização */}
         {children}
       </div>
--- FrontEnd/src/features/CameraCapture/index.tsx
+++ FrontEnd/src/features/CameraCapture/index.tsx
@@ -2,7 +2,7 @@ import React, { useRef, useState } from "react";
 import Webcam from "react-webcam";
 import { useCameraStore } from "./store/CameraStore";
 import { ImageCaptureModal } from "./components"; // Importe o componente modal
-import { CameraOutlined, LeftOutlined } from "@ant-design/icons";
+import { CameraOutlined, LeftOutlined, ExpandOutlined } from "@ant-design/icons";
 import { useNavigate } from "react-router-dom";
 
 const CameraCapture: React.FC = () => {
@@ -23,24 +23,29 @@ const CameraCapture: React.FC = () => {
     setModalVisible(false);
   };
   const webcamStyles = {
-    width: "100%", // 100% da largura da tela
-    height: "100vh", // 100% da altura da tela
+    width: "40rem",
+    height: "40rem", // 100% da largura da tela
+    // 100% da altura da tela
+  };
+  const webCamIcon = {
+    fontSize: "15rem",
   };
   //full
   return (
-    <div>
+    <div className="w-full h-full flex items-center ">
       <button
         onClick={() => {
           navigate(-1);
         }}
-        className="flex  z-10 fixed top-10 transform translate-x-1/4 p-4 bg-emerald-500 rounded-full shadow-lg text-white text-xl"
+        className="flex  z-10 fixed top-10 transform translate-x-1/4 p-4 bg-emerald-800 rounded-full shadow-lg text-white text-xl"
       >
         <LeftOutlined />
       </button>
-      <div className="flex flex-col items-center ">
+      <ExpandOutlined className={`flex z-10 fixed  right-1/2 transform translate-x-1/2 p-4  text-emerald-800 opacity-50 rounded-full shadow-lg `} style={webCamIcon} />
+      <div className="flex flex-col items-center justify-center bg-emerald-500 px-2" style={{ width: "100%", height: "100vh" }}>
         <Webcam audio={false} ref={webcamRef} style={webcamStyles} />
       </div>
-      <button onClick={captureImage} className="flex  z-10 fixed bottom-10 right-1/2 transform translate-x-1/2 p-4 bg-emerald-500 rounded-full shadow-lg text-white text-2xl">
+      <button onClick={captureImage} className="flex z-10 fixed bottom-10 right-1/2 transform translate-x-1/2 p-4 bg-emerald-800 rounded-full shadow-lg text-white text-2xl">
         <CameraOutlined />
       </button>
 
